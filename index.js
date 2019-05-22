dee1afe3eb5568ff39a27a227448137e3404e150
lucasflomuller <lucasflorianomuller@gmail.com>
2019-05-22 16:21:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"))
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
